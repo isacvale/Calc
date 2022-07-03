fb67b1ddc6bd8b69e821795653f4cf19d8e4ce65
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-07-03 15:09:45 -0300

    feat(calculator): use background

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
 import { Navigation } from '../../core'
+import { Background } from './background'
+
+const StyledMain = styled.main`
+  margin: 2em auto;
+`
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <div><h1>Welcome to the Home Page</h1></div>
-    </div>
+    <>
+      <Background mode="light"></Background>
+      <StyledMain className="App">
+        <Navigation />
+        <div><h1>Welcome to the Home Page</h1></div>
+      </StyledMain>
+    </>
   )
 }
 
