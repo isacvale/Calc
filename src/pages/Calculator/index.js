1a8490b8e58d58d25981e80c82b351cd104ca318
isacvale <isac.dvo@gmail.com>
2022-07-03 02:05:20 -0700

    Separate pages into a pages folder.

---
+++ src/pages/Calculator/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navigation } from '../../core'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <div><h1>Welcome to the Home Page</h1></div>
+    </div>
+  )
+}
+
+export default App
