e1e804b4a0fa7e822af806d5f00d9784489aa8ad
Nelson do Vale Oliveira <noliveira@ginger.io>
2022-07-08 10:58:49 -0300

    style: no unnecessary semi-colon

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit'
 import calcReducer from '../features/calc/calcSlice'
 
 export const store = configureStore({
   reducer: {
     calc: calcReducer,
   },
-});
+})
